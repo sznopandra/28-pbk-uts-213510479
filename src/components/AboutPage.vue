b54b44dbf6c94d53df319e9837ddee7d0835756f
sznopandra <93900661+sznopandra@users.noreply.github.com>
2023-05-08 06:41:57 +0700

    Update AboutPage.vue

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -1,56 +1,74 @@
 <template>
-    <div class="about-container">
-      <h1>About To Do App</h1>
-      <p>
-        To Do App adalah aplikasi sederhana untuk mengelola daftar tugas Anda.
-        Dengan To Do App, Anda dapat dengan mudah menambahkan, mengedit, dan menghapus tugas.
-      </p>
-      <h2>Fitur</h2>
-      <ul>
-        <li>Menambahkan tugas baru</li>
-        <li>Mengedit tugas yang sudah ada</li>
-        <li>Menghapus tugas</li>
-        <li>Menandai tugas sebagai selesai atau belum selesai</li>
-        <li>Menampilkan tugas berdasarkan kategori</li>
-      </ul>
-      <h2>Teknologi</h2>
-      <p>
-        To Do App dibangun dengan menggunakan teknologi berikut:
-      </p>
-      <ul>
-        <li>Vue.js</li>
-        <li>Vue Router</li>
-        <li>Bootstrap</li>
-        <li>LocalStorage API</li>
-      </ul>
-    </div>
-  </template>
-  
-  <style>
-  .about-container {
-    max-width: 800px;
-    margin: 0 auto;
-    padding: 20px;
-  }
-  
-  h1 {
-    font-size: 2rem;
-    margin-bottom: 20px;
-  }
-  
-  h2 {
-    font-size: 1.5rem;
-    margin-top: 40px;
-    margin-bottom: 10px;
-  }
-  
-  ul {
-    margin: 0;
-    padding: 0;
-    list-style: none;
-  }
-  
-  li {
-    margin-bottom: 10px;
-  }
-  </style>
+  <div class="about-container">
+    <h1>About To Do App</h1>
+    <p>
+      To Do App adalah aplikasi sederhana untuk mengelola daftar tugas Anda.
+      Dengan To Do App, Anda dapat dengan mudah menambahkan, mengedit, dan menghapus tugas.
+    </p>
+    <h2>Fitur</h2>
+    <ul>
+      <li>Menambahkan tugas baru</li>
+      <li>Mengedit tugas yang sudah ada</li>
+      <li>Menghapus tugas</li>
+      <li>Menandai tugas sebagai selesai atau belum selesai</li>
+      <li>Menampilkan tugas berdasarkan kategori</li>
+    </ul>
+    <h2>Teknologi</h2>
+    <p>
+      To Do App dibangun dengan menggunakan teknologi berikut:
+    </p>
+    <ul>
+      <li>Vue.js</li>
+      <li>Vue Router</li>
+      <li>Bootstrap</li>
+      <li>LocalStorage API</li>
+    </ul>
+  </div>
+</template>
+
+<style>
+.about-container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+  background-color: #f6f6f6;
+  border-radius: 10px;
+  box-shadow: 0px 3px 5px rgba(0,0,0,0.1);
+  color: #333;
+}
+
+h1 {
+  font-size: 2.5rem;
+  margin-bottom: 20px;
+  text-align: center;
+  text-transform: uppercase;
+  color: #1c1c1c;
+  letter-spacing: 1px;
+}
+
+h2 {
+  font-size: 2rem;
+  margin-top: 40px;
+  margin-bottom: 10px;
+  color: #1c1c1c;
+}
+
+ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+li {
+  margin-bottom: 10px;
+  font-size: 1.2rem;
+  color: #666;
+  font-weight: bold;
+}
+
+p {
+  font-size: 1.2rem;
+  line-height: 1.5;
+  margin-bottom: 30px;
+}
+</style>
