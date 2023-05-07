85e8eef49d33565be46587650f379495e05854a2
sznopandra <93900661+sznopandra@users.noreply.github.com>
2023-05-08 06:37:41 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,23 @@
+
+Berikut adalah contoh tampilan yang lebih menarik untuk codingan di atas:
+
 <template>
   <div id="app">
     <div class="welcome-msg">
-      <h1>Selamat Datang di Website Kami</h1>
+      <h1>Selamat Datang di Aplikasi To Do App </h1>
     </div>
     <div class="nav-links">
-      <router-link to="/Home">Home</router-link>
-      <router-link to="/About">About</router-link>
+      <router-link to="/Home" class="router-link">Daftar Tugas</router-link>
+      <router-link to="/About" class="router-link">Tentang Aplikasi</router-link>
     </div>
     <router-view></router-view>
   </div>
 </template>
-
 <script>
 export default {
   name: 'App'
 }
 </script>
-
 <style>
 #app {
   display: flex;
@@ -25,6 +26,7 @@ export default {
   justify-content: center;
   height: 100vh;
   font-family: Arial, sans-serif;
+  background-color: #f2f2f2;
 }
 
 .welcome-msg {
@@ -33,30 +35,41 @@ export default {
 }
 
 h1 {
-  font-size: 36px;
+  font-size: 48px;
   color: #333;
-  text-shadow: 1px 1px #fff;
+  text-shadow: 2px 2px #fff;
 }
 
 .nav-links {
   display: flex;
   justify-content: center;
-  margin-bottom: 20px;
+  margin-bottom: 30px;
 }
 
 .router-link {
-  margin: 0 10px;
-  font-size: 18px;
+  margin: 0 20px;
+  font-size: 24px;
   color: #333;
   text-decoration: none;
   transition: all 0.3s ease-in-out;
+  border-bottom: 2px solid transparent;
+  padding-bottom: 10px;
 }
 
 .router-link:hover {
   color: #fff;
   background-color: #333;
   border-radius: 5px;
-  padding: 5px 10px;
+  padding: 5px 20px;
+  border-bottom: 2px solid #fff;
+}
+
+.router-link-active {
+  color: #fff;
+  background-color: #333;
+  border-radius: 5px;
+  padding: 5px 20px;
+  border-bottom: 2px solid #fff;
 }
 
 </style>
