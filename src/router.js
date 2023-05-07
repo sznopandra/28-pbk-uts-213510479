04f674b21a9863798dc6ecd8780a4f140dc5ad75
sznopandra <93900661+sznopandra@users.noreply.github.com>
2023-05-08 06:17:28 +0700

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import Home from '@/components/Home.vue'
-import About from '@/components/About.vue'
+import Home from '@/components/HomePage.vue'
+import About from '@/components/AboutPage.vue'
 
 Vue.use(VueRouter)
 
