a50925508e75fab4868992721e5e5be2f43779df
sznopandra <93900661+sznopandra@users.noreply.github.com>
2023-05-08 06:15:43 +0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import Home from '@/components/Home.vue'
-import About from '@/components/About.vue'
+import HomePage from '@/components/HomePage.vue'
+import AboutPage from '@/components/AboutPage.vue'
 
 Vue.use(VueRouter)
 
